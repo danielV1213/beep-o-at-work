0677a52a9c3c632627e90ea03b0b8bb505f6e3a8
Nicolas29Diaz <nicolas_san.diaz@uao.edu.co>
2023-03-30 23:40:12 -0500

    Escena Creativo creada

---
+++ Assets/Control/PruebaAnchor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UltimateXR.Manipulation;
+using UnityEngine;
+
+public class PruebaAnchor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+
+}
